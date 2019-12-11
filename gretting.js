624b61e279881424b0a4c7982ffb95b53ecaf85a
junblee <topguide82@gmail.com>
2019-12-11 14:40:13 +0900

    function del

--- gretting.js
+++ gretting.js
@@ -5,20 +5,6 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
     SHOWING_CN = "showing";
 
-
-function paintGreeting(text){
-    form.classList.remove(SHOWING_CN);
-    greeting.classList.add(SHOWING_CN);
-    greeting.innerText = `Hello!! ${text}`
-}
-
-function loadName(){
-    const currentUser = localStorage.getItem(USER_LS);
-    if(currentUser === null){
-    } else {
-        paintGreeting(currentUser);
-    }
-}
 function init(){
     loadName();
 }
